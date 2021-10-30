a941af337ef0cba6ba803c3434d5d966b4f90c8d
LuisEEduardo <luiseduardolemos282@gmail.com>
2021-10-30 19:44:27 -0300

    add infomacoes a classe genero

--- TechBeauty.Dominio/Modelo/Cliente.cs
+++ TechBeauty.Dominio/Modelo/Cliente.cs
@@ -5,28 +5,15 @@ namespace TechBeauty.Dominio.Modelo
 {
     public class Cliente : Pessoa
     {
-
         public static Cliente Criar(int id, string nome, string cpf, DateTime dataNascimento, List<Contato> contatos)
         {
             Cliente cliente = new Cliente();
-            cliente.AlteraId(id);
             cliente.Id = id;
             cliente.Nome = nome;
             cliente.CPF = cpf;
             cliente.DataNascimento = dataNascimento;
             cliente.Contatos = contatos;
-            return cliente; 
+            return cliente;
         }
-
-        public void Alterar(string nome, string cpf, DateTime dataNascimento, List<Contato> contatos)
-        {
-            Nome = nome; 
-            CPF = cpf;
-            DataNascimento = dataNascimento;
-            Contatos = contatos;
-        }
-
-
-
     }
 }
