c513f3fc55424699821687c75362216fa5427109
LuisEEduardo <luiseduardolemos282@gmail.com>
2021-11-08 15:11:40 -0300

    add api

---
+++ TechBeauty.Api/Controllers/CargoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechBeauty.Dados.Repositorio;
+using TechBeauty.Dominio.Modelo;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechBeauty.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoController : ControllerBase
+    {
+        
+        CargoRepositorio cargoBD;
+
+        public CargoController()
+        {
+            cargoBD = new CargoRepositorio();
+        }
+
+        // GET: api/<CargoController>
+        [HttpGet]
+        public IEnumerable<Cargo> Get()
+        {
+            return cargoBD.SelecionarTudo();
+        }
+
+        // GET api/<CargoController>/5
+        [HttpGet("{id}")]
+        public Cargo Get(int id)
+        {
+            return cargoBD.Selecionar(id);
+        }
+
+        // POST api/<CargoController>
+        [HttpPost]
+        public void Post(string nome, string descricao)
+        {
+            cargoBD.Incluir(Cargo.Criar(nome, descricao));
+        }
+
+        // PUT api/<CargoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, string nome, string descricao)
+        {
+            cargoBD.Alterar2(id, nome, descricao, cargoBD.SelecionarTudo());
+        }
+
+        // DELETE api/<CargoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            cargoBD.Excluir(id);
+        }
+    }
+}
--- TechBeauty.Dados/Repositorio/CargoRepositorio.cs
+++ TechBeauty.Dados/Repositorio/CargoRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using TechBeauty.Dominio.Modelo;
 
 namespace TechBeauty.Dados.Repositorio
@@ -12,14 +13,25 @@ namespace TechBeauty.Dados.Repositorio
         }
 
         public override void Incluir(Cargo entity)
-        {            
+        {
             if (!ConsultaPorNome(entity.Nome))
             {
                 base.Incluir(entity);
             }
         }
 
-        
+        public void Alterar2(int id, string nome, string descricao, List<Cargo> lista)
+        {
+            foreach (var cargo in lista)
+            {
+                if (cargo.Id == id)
+                {
+                    cargo.MudarNome(nome);
+                    cargo.MudarDescricao(descricao);
+                }
+            }
+
+        }
 
     }
 }
